05a9d38b3e84a36f48ba319c22ecd6b3e7a1122d
Pepe Cano <ppcanodehuelva@gmail.com>
2013-03-17 23:35:12 +0100

    Add HeaderCloseButton for Android

--- lib-android/main.js
+++ lib-android/main.js
@@ -1,2 +1,3 @@
 
 require('yoin-views/lib-android/views/uis/buttons/back_button');
+require('yoin-views/lib-android/views/uis/buttons/header_close_button');
---
+++ lib-android/views/uis/buttons/header_close_button.js
@@ -0,0 +1,6 @@
+
+Yn.HeaderCloseButtonView = Em.View.extend(Yn.Btap, {
+  action: 'close',
+  classNames: ['android-back-button', 'icon']
+
+});
